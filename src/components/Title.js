4f85f47827057d04b930046bcaa475120e1bce9b
RebekahEloise <rebekaheloisemiller@gmail.com>
2023-02-26 14:52:51 -0600

    hero img

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = (word1, word2) => {
+  return (
+    <div className="section-title">
+    <h2>{word1} <span>{word2}</span></h2>
+  </div>
+  )
+}
+
+export default Title
